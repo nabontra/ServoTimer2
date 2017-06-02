adad1601ecec41b23b6a670c4b811c66ef0cf784
irubio282 <irubio282@gmail.com>
2017-06-02 01:07:00 -0700

    Update ServoTimer2.h

--- ServoTimer2.h
+++ ServoTimer2.h
@@ -73,8 +73,8 @@ The pulse width timing is accurate to within 1%
 #define ServoTimer2_h
 
 #include <inttypes.h>
-typedef uint8_t boolean;
-typedef uint8_t byte;
+//typedef uint8_t boolean;
+//typedef uint8_t byte;
 
 #define MIN_PULSE_WIDTH       750        // the shortest pulse sent to a servo  
 
